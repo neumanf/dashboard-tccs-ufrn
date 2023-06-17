c34b4a9810a4d5eef380e87359e897aad3e1f1ab
neumanf <fabricionewman@protonmail.com>
2023-06-17 12:03:16 -0300

    feat: add top 5 courses in regards to works production graph

--- dashboard.py
+++ dashboard.py
@@ -11,26 +11,36 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
+# Import data and drop rows with missing values, if any
+df = pd.read_csv('tccs.csv', sep=';')
+df.dropna(inplace=False)
+
+# As it has many inconsistent dates, it was necessary to remove the column so as not to affect the analyses
+df = df.drop('data_defesa', axis=1)
+
+df['ano'] = df['ano'].astype(str)
+df['ano'] = df['ano'].str.split('.').str[0]
+
+# Get top 5 courses in regards to works production
+num_papers_by_course = df['curso'].value_counts().head(5)
+
 df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+    "Curso": num_papers_by_course.index,
+    "Número de trabalhos": num_papers_by_course.values,
 })
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+top_5_courses = px.bar(df, x="Curso", y="Número de trabalhos")
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='Trabalhos de Conclusão de Curso (UFRN)'),
 
     html.Div(children='''
-        Dash: A web application framework for Python.
+        Top 5 cursos que mais entregam trabalho
     '''),
 
     dcc.Graph(
-        id='example-graph',
-        figure=fig
+        id='top-5-courses-graph',
+        figure=top_5_courses
     )
 ])
 
