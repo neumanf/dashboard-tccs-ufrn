b160eb59dcd1c67977e679dac67da0b6c3dbfdae
neumanf <fabricionewman@protonmail.com>
2023-06-17 13:42:19 -0300

    feat: add course with most papers produced in the last 5 years graph

--- dashboard.py
+++ dashboard.py
@@ -24,12 +24,25 @@ df['ano'] = df['ano'].str.split('.').str[0]
 # Get top 5 courses in regards to works production
 num_papers_by_course = df['curso'].value_counts().head(5)
 
-df = pd.DataFrame({
+num_papers_by_course_df = pd.DataFrame({
     "Curso": num_papers_by_course.index,
     "Número de trabalhos": num_papers_by_course.values,
 })
 
-top_5_courses = px.bar(df, x="Curso", y="Número de trabalhos")
+top_5_courses_graph = px.bar(num_papers_by_course_df, x="Curso", y="Número de trabalhos")
+
+# Course with most papers produced in the last 5 years
+current_year = 2023
+last_five_years = df[df['ano'].str.split('.').str[0].astype(int) >= current_year - 5]
+course_with_most_papers = last_five_years['curso'].value_counts().index[0]
+course_last_five_years = last_five_years[last_five_years['curso'] == course_with_most_papers].groupby(['ano']).size().reset_index(name='works_count')
+
+course_with_most_papers_df = pd.DataFrame({
+    "Ano": course_last_five_years.ano,
+    "Número de trabalhos": course_last_five_years.works_count,
+})
+
+course_with_most_papers_graph = px.bar(course_with_most_papers_df, x="Ano", y="Número de trabalhos", color="Ano")
 
 app.layout = html.Div(children=[
     html.H1(children='Trabalhos de Conclusão de Curso (UFRN)'),
@@ -40,7 +53,16 @@ app.layout = html.Div(children=[
 
     dcc.Graph(
         id='top-5-courses-graph',
-        figure=top_5_courses
+        figure=top_5_courses_graph
+    ),
+
+    html.Div(children=f'''
+        Distribuição de trabalhos do curso {course_with_most_papers} nos últimos 5 anos
+    '''),
+
+    dcc.Graph(
+        id='course-with-most-papers-produced-in-the-last-5-years-graph',
+        figure=course_with_most_papers_graph
     )
 ])
 
